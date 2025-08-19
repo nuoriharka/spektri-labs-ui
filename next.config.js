c2948360b7db9cd9a57dab31efc57302310c9535
nuoriharka <lauri.rainio@outlook.com>
2025-08-19 13:24:58 +0000

    next/image: allow html.tailus.io and placehold.co

--- next.config.js
+++ next.config.js
@@ -6,6 +6,10 @@ const nextConfig = {
 	images: {
 		formats: ["image/avif", "image/webp"],
 		deviceSizes: [360, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+		remotePatterns: [
+			{ protocol: 'https', hostname: 'html.tailus.io' },
+			{ protocol: 'https', hostname: 'placehold.co' },
+		],
 	},
 }
 
